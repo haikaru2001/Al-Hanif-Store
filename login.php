fad45976e67918567f8d4d3b88200680b4a00959
El Thaariq Is'ad <eriq.isad@gmail.com>
2023-01-14 10:40:13 +0700

    四
    Fixing.

--- login.php
+++ login.php
@@ -81,11 +81,11 @@
 									SIGN IN</button>
 								
 							</form>
-							<hr>
+							<!-- <hr>
 							<div class="text-center">
 								<a class="small" href="forgot-password.html">Forgot Password?</a>
 								<a class="small" href="register.html">Create an Account!</a>
-							</div>
+							</div> -->
 						</div>
                     </div>
                 </div>
