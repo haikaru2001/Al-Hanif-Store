aabdc9260282c2a4e8c6220172b786b62c6c9ac9
El Thaariq Is'ad <eriq.isad@gmail.com>
2023-01-14 11:30:22 +0700

    Fixing

--- admin/template/sidebar.php
+++ admin/template/sidebar.php
@@ -124,7 +124,7 @@
                             Profile
                         </a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php">
+                        <a class="dropdown-item" href="logout.php" onclick="javascript:return confirm('Yakin Logout?');">                    
                             <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                             Logout
                         </a>
